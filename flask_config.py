e4a220e2833d5236f42167f1b7e2690732e7adb7
Michael Kreisel <Kreisel.Michael@epa.gov>
2022-01-11 11:58:53 -0500

    clean up unnecessary Flask config params

--- flask_config.py
+++ flask_config.py
@@ -1,11 +1,5 @@
 class Config:
     """Set Flask configuration from .env file."""
-
-    # General Config
-    SECRET_KEY = "test"
-    FLASK_ENV = "dev"
-
     # Database
-    #SQLALCHEMY_DATABASE_URI = r'test.db'
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
