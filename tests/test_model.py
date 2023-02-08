032bf83f818a62eea1bfe911c2177bea8a942b6c
Michael Kreisel <Kreisel.Michael@epa.gov>
2023-02-08 13:44:28 -0500

    add test coverage packages

--- tests/test_model.py
+++ tests/test_model.py
@@ -17,9 +17,9 @@ def test_model_output(hf_model):
     predictions, default_schedule = hf_model.predict(text=text, doc_type=doc_type, prediction_metadata=prediction_metadata, has_capstone=has_capstone, keywords=keywords, subjects=subjects, attachments=attachments)
     assert len(predictions) == 3
     assert predictions[0].schedule == data_classes.RecordSchedule(function_number='301', schedule_number='1016', disposition_number='c')
-    assert predictions[0].probability == 0.45385774970054626
+    assert abs(predictions[0].probability - 0.45385774970054626) < 0.0001
     assert predictions[1].schedule == data_classes.RecordSchedule(function_number='306', schedule_number='1023', disposition_number='c')
-    assert predictions[1].probability == 0.1347094178199768
+    assert abs(predictions[1].probability - 0.1347094178199768) < 0.0001
     assert predictions[2].schedule == data_classes.RecordSchedule(function_number='108', schedule_number='1044', disposition_number='d')
-    assert predictions[2].probability == 0.11839142441749573
+    assert abs(predictions[2].probability - 0.11839142441749573) < 0.0001
     assert default_schedule == None
